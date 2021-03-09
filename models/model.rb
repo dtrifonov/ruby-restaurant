907da838ea26ca458109274d5a1da7aad9430a27
Kaloyan Kostadinov <kaloyan.kostadinov@mentormate.com>
2021-03-09 17:24:07 +0200

    Updated static method syntax

--- models/model.rb
+++ models/model.rb
@@ -3,8 +3,11 @@ require './config/app_config'
 
 class Model
 
-  def self.app_data
-    config = AppConfig.get_config
-    AppData.new(config["data"]["file"])
+  class << self
+    def app_data
+      config = AppConfig.get_config
+      AppData.new(config["data"]["file"])
+    end
   end
+
 end
